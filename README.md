4475a965e5aec32321a1483d3e65d3f7d2a800ab
Quentin Decoopman <115376315+QDec59@users.noreply.github.com>
2024-01-28 16:09:00 +0100

    create mini BDD +readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# Api
+
+Petite Api poour projet perso
+
+## Demo
+
+[Lien vers l'api](<[URL](https://api-e79o.onrender.com/)>)
+
+## Getting Started
+
+npm start
+
+## Prerequisites
+
+Node.js
+npm
+
+## Built With
+
+- [Express](https://expressjs.com/)
+- [Node](https://nodejs.org/)
+- [Mise en ligne](https://www.render.com/)
+
+<!-- ## References
+
+URL
+
+## Author
+
+[link text](URL) -->
+
+## License
+
+[The MIT License](https://opensource.org/licenses/MIT).
--- index.js
+++ index.js
@@ -1,11 +1,86 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
+
+app.get("/", (req, res) => {
+  res.send(`
+    <div style="margin: 5em auto; width: 400px; line-height: 1.5">
+      <h1 style="text-align: center">Hello!</h1>
+      <p>Si tu vois ce message, c'est que ton serveur est bien lancé !</p>
+      <div>Désormais, tu dois venir utiliser l'API</div>
+      <ul style="display: inline-block; margin-top: .2em">
+        <li><code>POST https://api-e79o.onrender.com/login</code></li>
+        
+      </ul>
+    </div>
+  `);
+});
+
+app.post("/login", (req, res) => {
+  console.log("POST /login", request.body);
+
+  const { email, password } = request.body;
+
+  let username;
+  if (db.users[email] && db.users[email].password === password) {
+    username = db.users[email].username;
+  }
+
+  if (username) {
+    console.log("200 OK", username);
+    res.json({
+      pseudo: username,
+    });
+  } else {
+    console.log("401 UNAUTHORIZED");
+    res.status(401).end();
+  }
+});
+
+app.post("/forgot", (req, res) => {
+  const { email } = request.body;
+  if (db.users[email]) {
+    res.json({
+      pseudo: db.users[email].username,
+    });
+  } else {
+    res.status(400).end();
+  }
+});
+
+const db = {
+  users: [
+    {
+      id: 1,
+      name: "John",
+      password: 123,
+    },
+    {
+      id: 2,
+      name: "Jane",
+      password: 123,
+    },
+    {
+      id: 3,
+      name: "Joe",
+      password: 123,
+    },
+  ],
+};
 
 app.listen(PORT, () => {
   console.log(`listening on http://localhost:${PORT}`);
