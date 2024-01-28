4ad1cd932f3136e3531d8b56357c904e22d8093f
Quentin Decoopman <115376315+QDec59@users.noreply.github.com>
2024-01-28 16:48:09 +0100

    refact

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 
 const PORT = 3000;
-
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
 
 app.get("/", (req, res) => {
   res.send(`
@@ -23,43 +10,14 @@ app.get("/", (req, res) => {
       <p>Si tu vois ce message, c'est que ton serveur est bien lancé !</p>
       <div>Désormais, tu dois venir utiliser l'API</div>
       <ul style="display: inline-block; margin-top: .2em">
-        <li><code>POST https://api-e79o.onrender.com/login</code></li>
-        
+        <li><code>GET https://api-e79o.onrender.com/users</code></li>
       </ul>
     </div>
   `);
 });
 
-app.post("/login", (req, res) => {
-  console.log("POST /login", request.body);
-
-  const { email, password } = request.body;
-
-  let username;
-  if (db.users[email] && db.users[email].password === password) {
-    username = db.users[email].username;
-  }
-
-  if (username) {
-    console.log("200 OK", username);
-    res.json({
-      pseudo: username,
-    });
-  } else {
-    console.log("401 UNAUTHORIZED");
-    res.status(401).end();
-  }
-});
-
-app.post("/forgot", (req, res) => {
-  const { email } = request.body;
-  if (db.users[email]) {
-    res.json({
-      pseudo: db.users[email].username,
-    });
-  } else {
-    res.status(400).end();
-  }
+app.get("/users", (req, res) => {
+  res.send(db.users);
 });
 
 const db = {
